8aea1e5d81291b3e29e1157b96cc245c9ea226fb
Omar Villanueva <o.villanueva@thearcane.com>
2020-05-16 08:57:09 -0600

    Completed the workshop

---
+++ app/src/constants.js
@@ -0,0 +1,13 @@
+export const ICONS = ["fish", "poop", "weather"];
+export const TICK_RATE = 2000;
+export const SCENES = ["day", "rain"];
+export const RAIN_CHANCE = 0.2;
+export const DAY_LENGTH = 30;
+export const NIGHT_LENGTH = 4;
+
+export const getNextHungerTime = (clock) =>
+  Math.floor(Math.random() * 3) + 5 + clock;
+export const getNextDieTime = (clock) =>
+  Math.floor(Math.random() * 2) + 3 + clock;
+export const getNextPoopTime = (clock) =>
+  Math.floor(Math.random() * 3) + 4 + clock;
--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,10 @@
-import gameState from "./gameState";
-
-const TICK_RATE = 3000;
+import { TICK_RATE } from "./constants";
+import gameState, { handleUserAction } from "./gameState";
+import initButtons from "./buttons";
 
 async function init() {
   console.log("starting game");
+  initButtons(handleUserAction);
 
   let nextTimeToTick = Date.now();
 
